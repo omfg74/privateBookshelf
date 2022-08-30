a9c0f63e854094610e539a89b9313c026df38c07
aleksandr <podsh74@gmail.com>
2022-08-31 00:41:19 +0500

    bugfix:env fix

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 docker-compose -f docker-compose.run.yaml -p book-shelf down
+ex -s +"g/VERSION/d" -cwq .env
+VERSION=$(git describe --abbrev=0 --tags)
+echo ${VERSION}
+ECHO APP_VERSION="${VERSION}" >>.env
 docker-compose -f docker-compose.run.yaml -p book-shelf --env-file .env up -d
