901beb1070009ce550056b3c7dc47d4cc2d6d314
aleksandr <podsh74@gmail.com>
2022-09-01 01:46:05 +0500

    bugfix:prod build fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker-compose -f docker-compose.run.yaml -p book-shelf down
+docker-compose -f docker-compose.build.yaml -p book-shelf down
 ex -s +"g/VERSION/d" -cwq .env
 VERSION=$(git describe --abbrev=0 --tags)
 echo ${VERSION}
 echo APP_VERSION="${VERSION}" >>.env
-docker-compose -f docker-compose.run.yaml -p book-shelf --env-file .env up -d
+docker-compose -f docker-compose.build.yaml build --no-cache && docker-compose -f docker-compose.build.yaml -p book-shelf --env-file .env up --force-recreate
