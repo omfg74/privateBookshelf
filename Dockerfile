0a7fc85792416d661a9f9092bb73f0068801cc3d
aleksandr <podsh74@gmail.com>
2022-09-01 23:28:50 +0500

    bugfix:prod build fix

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,12 @@ FROM runtime
 
 RUN mkdir /runtime
 RUN chmod -R 777 /runtime
+RUN mkdir /source
+WORKDIR /source
+COPY . /source
+RUN ./gradlew clean
+RUN ./gradlew vaadinClean
+RUN ./gradlew vaadinBuildFrontend
+RUN ./gradlew bootJar
+RUN chmod -R 777 /runtime
 COPY build/libs/book-shelf.jar /runtime
--- deploy_arm.sh
+++ deploy_arm.sh
@@ -1,10 +1,16 @@
-if [[ "$(docker images -q runtime 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q runtime 2>/dev/null)" == "" ]]; then
   docker build -t runtime deploy/.
 fi
-./gradlew clean
-./gradlew vaadinClean
-./gradlew vaadinBuildFrontend
-./gradlew bootJar
+docker-compose -f docker-compose.armv7.yaml -p book-shelf down
+
+docker image rm $(docker images -q book-shelf_java-runtime) --force
+docker image rm $(docker images -q book-shelf_db_init) --force
+
+docker rm -q runtime -y
+#./gradlew clean
+#./gradlew vaadinClean
+#./gradlew vaadinBuildFrontend
+#./gradlew bootJar
 docker-compose -f docker-compose.armv7.yaml -p book-shelf down
 ex -s +"g/VERSION/d" -cwq .env
 VERSION=$(git describe --abbrev=0 --tags)
